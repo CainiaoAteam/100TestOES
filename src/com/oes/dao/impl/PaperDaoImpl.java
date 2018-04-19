d7320d357b2c6c76acb943dedf67a0154bf6eaf6
Hking <Hking@GuoGuanzhi>
2018-04-20 00:25:22 +0800

    application配置文件格式整理 郭冠志

--- src/com/oes/dao/impl/PaperDaoImpl.java
+++ src/com/oes/dao/impl/PaperDaoImpl.java
@@ -6,11 +6,13 @@ import com.oes.bean.TestPaper;
 import com.oes.dao.PaperDao;
 
 public class PaperDaoImpl extends HibernateDaoSupport implements PaperDao {
-
+	/**
+	 * 不要实现该方法
+	 * @param pid
+	 * @return
+	 */
 	public TestPaper getPaperByPid(String pid) {
 		// TODO Auto-generated method stub
-		
-		
 		
 		return null;
 	}
